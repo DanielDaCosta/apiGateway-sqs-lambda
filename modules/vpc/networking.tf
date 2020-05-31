04cfc9bfa090f70c99b5bb147d89a3b018fa234c
Daniel Da Costa <daniel.pereiracosta@hotmail.com>
2020-05-31 20:58:46 -0300

    Creating folders

---
+++ modules/vpc/networking.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "${var.tenancy}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "${var.subnet_cidr}"
+
+  tags = {
+    Name = "Main"
+  }
+}
---
+++ modules/vpc/vars.tf
@@ -0,0 +1,17 @@
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "tenancy" {
+    default = "tenancy"
+}
+
+variable "vpc_id" {
+    default = ""
+}
+
+variable "subnet_cidr" {
+    default = ""
+}
+
