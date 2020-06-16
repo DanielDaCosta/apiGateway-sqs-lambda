804af1a9244e962e5306fa5b76b376b6b869d270
Daniel Da Costa <daniel.pereiracosta@hotmail.com>
2020-06-16 16:25:37 -0300

    SQS integration deployed and tested

--- apiGateway.tf
+++ apiGateway.tf
@@ -44,13 +44,50 @@ resource "aws_api_gateway_integration" "api" {
     "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
   }
 
+  # Request Template for passing Method, Body, QueryParameters and PathParams to SQS messages
   request_templates = {
-    "application/json" = "Action=SendMessage&MessageBody=$input.body"
+    "application/json" = <<EOF
+Action=SendMessage&MessageBody={
+  "method": "$context.httpMethod",
+  "body-json" : $input.json('$'),
+  "queryParams": {
+    #foreach($param in $input.params().querystring.keySet())
+    "$param": "$util.escapeJavaScript($input.params().querystring.get($param))" #if($foreach.hasNext),#end
+
+  #end
+  },
+  "pathParams": {
+    #foreach($param in $input.params().path.keySet())
+    "$param": "$util.escapeJavaScript($input.params().path.get($param))" #if($foreach.hasNext),#end
+    #end
+  }
+}"
+EOF
   }
 
   depends_on = [
     aws_iam_role_policy_attachment.api_exec_role
   ]
+}
+
+# Mapping SQS Response
+resource "aws_api_gateway_method_response" "http200" {
+  rest_api_id = aws_api_gateway_rest_api.apiGateway.id
+  resource_id = aws_api_gateway_resource.form_score.id
+  http_method = aws_api_gateway_method.method_form_score.http_method
+  status_code = 200
+}
+
+resource "aws_api_gateway_integration_response" "http200" {
+  rest_api_id       = aws_api_gateway_rest_api.apiGateway.id
+  resource_id       = aws_api_gateway_resource.form_score.id
+  http_method       = aws_api_gateway_method.method_form_score.http_method
+  status_code       = aws_api_gateway_method_response.http200.status_code
+  selection_pattern = "^2[0-9][0-9]"                                       // regex pattern for any 200 message that comes back from SQS
+
+  depends_on = [
+    aws_api_gateway_integration.api
+    ]
 }
 
 resource "aws_api_gateway_deployment" "api" {
@@ -60,4 +97,15 @@ resource "aws_api_gateway_deployment" "api" {
   depends_on = [
     aws_api_gateway_integration.api,
   ]
+
+  # Redeploy when there are new updates
+  triggers = {
+    redeployment = sha1(join(",", list(
+      jsonencode(aws_api_gateway_integration.api),
+    )))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
